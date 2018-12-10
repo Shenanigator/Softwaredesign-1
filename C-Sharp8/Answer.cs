69591b59b6532428ec437349bd1131ac7b1c1b94
Falco Böhnke <falco.boehnke@gmail.com>
2018-12-10 17:03:49 +0100

    Update Answer.cs

--- C-Sharp8/Answer.cs
+++ C-Sharp8/Answer.cs
@@ -4,6 +4,7 @@ namespace C_Sharp8
 {
     class Answer
     {
+        // Namen aussagekräfiter machen ("answerText", "isCorrect") macht Aufrufe aus anderen klassen klarerer
         public string text;
         bool correct;
     }
